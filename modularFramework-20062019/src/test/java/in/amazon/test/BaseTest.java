9e443a8af8b0153fe16e9cf2a4b73fffe5b60d36
Saurabh Dhingra <Saurabh Dhingra@192.168.1.9>
2019-06-26 22:44:30 +0530

    Added extent Report

--- modularFramework-20062019/src/test/java/in/amazon/test/BaseTest.java
+++ modularFramework-20062019/src/test/java/in/amazon/test/BaseTest.java
@@ -1,8 +1,16 @@
 package in.amazon.test;
 
+import java.util.Date;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import commonLibs.implementations.CommonDriver;
 import in.amazon.pages.Homepage;
@@ -18,8 +26,37 @@ public class BaseTest {
 
 	WebDriver driver;
 
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extentReport;
+	ExtentTest extentTest;
+
+	static String htmlReportFilename;
+	static String currentWorkingDirectory;
+	static String testExecutionStartTime;
+
+	static {
+		htmlReportFilename = "AmazonTestReport";
+		Date date = new Date();
+		testExecutionStartTime = String.valueOf(date.getTime());
+		currentWorkingDirectory = System.getProperty("user.dir");
+	}
+
+	@BeforeSuite
+	public void preSetup() {
+
+		String htmlReportPath = String.format("%s/reports/%s_%s.html", currentWorkingDirectory, htmlReportFilename,
+				testExecutionStartTime);
+		
+		htmlReporter = new ExtentHtmlReporter(htmlReportPath);
+		extentReport = new ExtentReports();
+		extentReport.attachReporter(htmlReporter);
+
+	}
+
 	@BeforeClass(alwaysRun = true)
 	public void setup() throws Exception {
+		
+		extentTest = extentReport.createTest("Set up - Setting up the pre requisites for test cases");
 
 		invokeBrowserAndNavigateToBaseUrl();
 
@@ -29,10 +66,11 @@ public class BaseTest {
 
 	@AfterClass(alwaysRun = true)
 	public void cleanUp() throws Exception {
-		
-		
+
 		closingBrowsers();
 		
+		extentReport.flush();
+
 	}
 
 	private void closingBrowsers() throws Exception {
@@ -41,20 +79,29 @@ public class BaseTest {
 	}
 
 	private void invokeBrowserAndNavigateToBaseUrl() throws Exception {
+		
 		url = "https://www.amazon.in/";
-
-		cmnDriver = new CommonDriver("chrome");
-
-		cmnDriver.setPageloadTimeout(30);
+		extentTest.log(Status.INFO, "Base URL - "+ url);
+		
+		String browserType = "chrome";
+		cmnDriver = new CommonDriver(browserType);
+		extentTest.log(Status.INFO, "Browser Invoked - "+ browserType);
+		
+		int pageloadTimeout = 30;
+		cmnDriver.setPageloadTimeout(pageloadTimeout);
+		extentTest.log(Status.INFO, "Page load timeout - "+ pageloadTimeout);
+		
 		cmnDriver.setElementDetectionTimeout(10);
 
 		cmnDriver.navigateToFirstUrl(url);
+		extentTest.log(Status.INFO, "Navigating to the URL - "+ url);
+		
 		driver = cmnDriver.getDriver();
 	}
 
 	private void initializingPages() {
 		homepage = new Homepage(driver);
-
+		extentTest.log(Status.INFO, "Initailizing pages ");
 		resultPage = new Resultspage(driver);
 
 	}
--- modularFramework-20062019/src/test/java/in/amazon/test/HomepageTest.java
+++ modularFramework-20062019/src/test/java/in/amazon/test/HomepageTest.java
@@ -1,16 +1,35 @@
 package in.amazon.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
 
 public class HomepageTest extends BaseTest {
 
-	
-
 	@Test
 	public void verifySearchProduct() throws Exception {
-		homepage.searchProduct("Apple Watch", "Electronics");
+
+		extentTest = extentReport.createTest("TC - 001 - Verify Search Product Functionality");
+
+		String product = "Apple watch";
+		String category = "Electronics";
+
+		extentTest.log(Status.INFO, "Searching product with category - " + product + ", " + category);
+		homepage.searchProduct(product, category);
 	}
 
-	
+	@Test
+	public void verifyTitleOfThePage() throws Exception {
+
+		extentTest = extentReport.createTest("TC - 003 - Verify the title of homepage");
+		
+		String expectedTitle = "Amazon Home Page";
+
+		String actualTitle = cmnDriver.getTitle();
+
+		extentTest.log(Status.INFO, "Actual Title - "+ actualTitle + " and expected Title - "+ expectedTitle);
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
 
 }
